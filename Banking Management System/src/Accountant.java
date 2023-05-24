9d4b73b097512c3f75d385221bdc4dc2d55f65e4
Ammar-Ahmed7 <ammar.ahmed9020@gmail.com>
2023-05-24 08:43:40 -0700

    Updated

--- Banking Management System/src/Accountant.java
+++ Banking Management System/src/Accountant.java
@@ -14,10 +14,15 @@ public class Accountant extends Acc_and_Mang_Abstract{
 
    public Accountant(){
         CreateAccount();
-        Login();
-        deposit();
-        withdraw();
-
+       // Login();
+       while(true) {
+           deposit();
+           withdraw();
+           BillProcess();
+           viewBalance();
+           TransferMoney();
+           ChangePin();
+       }
     }
 
     public void CreateAccount() {
@@ -50,7 +55,7 @@ public class Accountant extends Acc_and_Mang_Abstract{
         String pass=sc.next();
         Password=pass;
 
-        System.out.print("Congratulations Your "+Acctype+" has been successfully created !!! ");
+        System.out.print("\n Congratulations Your "+Acctype+" has been successfully created !!! ");
    }
 
     public String getName() {
@@ -70,21 +75,22 @@ public class Accountant extends Acc_and_Mang_Abstract{
     @Override
     public void Login() {
         Scanner sc=new Scanner(System.in);
-        System.out.print("\n Enter your name = ");
+        System.out.println("\n\n\t ACCOUNTANT LOGIN\n");
+        System.out.print("Enter your name = ");
         String n2=sc.next();
 
         System.out.print("\n Enter your Password = ");
         String pass2=sc.next();
 
-        System.out.println("Welcome !!!");
+        System.out.println("\n Welcome !!!");
     }
 
     public void deposit(){
        Scanner sc=new Scanner(System.in);
-        System.out.print("\nEnter the amount you want to deposit = ");
+        System.out.print("\n Enter the amount you want to deposit = ");
         double deposit=sc.nextDouble();
         Balance=Balance+deposit;
-        System.out.println("Your balance after deposit = "+getBalance());
+        System.out.println("\n Your balance after deposit = "+getBalance());
     }
 
     public void withdraw(){
@@ -92,31 +98,114 @@ public class Accountant extends Acc_and_Mang_Abstract{
         System.out.print("\n Your balance before withdrawal = "+getBalance());
         System.out.print("\nEnter the amount you want to withdraw = ");
         double withdraw=sc.nextDouble();
-        Balance=Balance-withdraw;
-        System.out.println("Your balance after withdraw = "+getBalance());
-    }
+        if(getBalance()<withdraw){
+            System.out.println("\n Insufficient Balance to withdraw ");
+        }else {
+            Balance = Balance - withdraw;
+            System.out.println("\n Your balance after withdraw = " + getBalance());
+        }
+        }
     public double getBalance() {
         return Balance;
     }
 
     public void BillProcess(){
        Scanner sc=new Scanner(System.in);
-       System.out.println("Enter your name = ");
+       System.out.print("\n Enter your name = ");
        String n3=sc.next();
        while(!n3.equals(getName())){
-           System.out.println("Invalid name \n Enter a valid registered name = ");
+           System.out.print("\n Invalid name \n Enter a valid registered name = ");
            n3=sc.next();
        }
-        System.out.println("Enter your CNIC = ");
+        System.out.print("\nEnter your CNIC = ");
         String id2=sc.next();
         while(!id2.equals(getCNIC())){
-            System.out.println("Invalid CNIC \n Enter a valid registered CNIC = ");
+            System.out.print("\n Invalid CNIC \n Enter a valid registered CNIC = ");
             id2=sc.next();
         }
-        System.out.println("Enter your Bill Number = ");
-        String bill=sc.next();
-        while()
+        System.out.print("\nEnter your Bill Number = ");
+        String billNo=sc.next();
+        System.out.print("\n Enter Bill amount = ");
+        double billam=sc.nextDouble();
+        if(getBalance()<billam){
+            System.out.println("\n Insufficient Balance to process the Bill. Please deposit some money into your account to process the Bill");
+        }else {
+            Balance = Balance - billam;
+            System.out.println("\n Bill Successfully has been Processed");
+        }
+    }
 
+    public void viewBalance(){
+       Scanner sc=new Scanner(System.in);
+        System.out.print("\n Enter your name = ");
+        String n4=sc.next();
+        while(!n4.equals(getName())){
+            System.out.print("\n Invalid name \n Enter a valid registered name = ");
+            n4=sc.next();
+        }
+        System.out.print("\n Enter your Password = ");
+        String pass3=sc.next();
+        while(!pass3.equals(getPassword())){
+            System.out.print("\n Invalid Password \n Enter a valid registered Password = ");
+            pass3=sc.next();
+        }
+        System.out.println("Your Balance is = "+getBalance());
+
+    }
+
+    public void TransferMoney(){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("\n Enter your name = ");
+        String n5=sc.next();
+        while(!n5.equals(getName())){
+            System.out.print("\n Invalid name \n Enter a valid registered name = ");
+            n5=sc.next();
+        }
+        System.out.print("\n Enter your Password = ");
+        String pass4=sc.next();
+        while(!pass4.equals(getPassword())){
+            System.out.print("\n Invalid Password \n Enter a valid registered Password = ");
+            pass4=sc.next();
+        }
+        System.out.print("\n Enter the Account Number you want to transfer money = ");
+        String accno=sc.next();
+
+        System.out.print("\n Enter the amount you want to deposit = ");
+        double am=sc.nextDouble();
+        if(getBalance()<am){
+            System.out.print("\n Insufficient Balance for transfering money");
+        }else{
+              Balance = Balance - am;
+            System.out.print("\n Money has been successfully Transferred ");
+            System.out.println("\n Your Balance after transfering money = "+getBalance());
+        }
+    }
+
+    public void ChangePin(){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("\n Enter your name = ");
+        String n6=sc.next();
+        while(!n6.equals(getName())){
+            System.out.print("\n Invalid name \n Enter a valid registered name = ");
+            n6=sc.next();
+        }
+        System.out.print("\n Enter your Current Password = ");
+        String pass5=sc.next();
+        while(!pass5.equals(getPassword())){
+            System.out.print("\n Invalid Password \n Enter a valid registered Password = ");
+            pass5=sc.next();
+        }
+        System.out.print("\n Enter your New Password = ");
+        String pass6=sc.next();
+
+        System.out.print("\n Confirm your New Password = ");
+        String pass7=sc.next();
+        while(!pass7.equals(pass6)){
+            System.out.print("\n Invalid Confirmation \n Enter new valid Password for Confirmation = ");
+            pass7=sc.next();
+        }
+        Password=pass7;
+        System.out.println("\n Your Password has been successfully changed ");
 
     }
 

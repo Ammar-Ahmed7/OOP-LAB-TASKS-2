7732470072711d1a6269c52a31a0580fefbff27d
Ammar-Ahmed7 <ammar.ahmed9020@gmail.com>
2023-06-02 19:59:35 +0500

    Updated

--- Banking_Management_System2/Banking_Management_System/src/main/java/com/example/banking_management_system/Create_Account.java
+++ Banking_Management_System2/Banking_Management_System/src/main/java/com/example/banking_management_system/Create_Account.java
@@ -2,12 +2,16 @@ package com.example.banking_management_system;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
-public class Create_Account {
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.sql.*;
+import java.time.LocalDate;
+
+
+public class Create_Account implements Initializable {
 
     @FXML
     private TextField AccountNo;
@@ -40,7 +44,7 @@ public class Create_Account {
     private Button back;
 
     @FXML
-    private Button edit;
+    private Button register;
 
     @FXML
     private RadioButton female;
@@ -57,8 +61,41 @@ public class Create_Account {
     }
 
     @FXML
-    void oneditClicked(ActionEvent event) {
+    void oneRegisterClicked(ActionEvent event) throws ClassNotFoundException, SQLException {
+        Connection dataBaselink;
+        String databaseUser="root";
+        String databasePassword="zamanlew6732";
+        String url="jdbc:mysql://localhost:3306/registrationpro";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        dataBaselink=DriverManager.getConnection(url,databaseUser,databasePassword);
+        PreparedStatement stat = dataBaselink.prepareStatement("INSERT Into sign_up  (full_name,phone,email,password,dob,gender,qualification,city) values (?,?,?,?,?,?,?,?)");
+
+        stat.setString(1,Name.getText());
+        LocalDate localDate = DOB.getValue();
+        Date date = Date.valueOf(localDate);
+        stat.setDate(2, date);
+        stat.setString(3,Country.getText());
+        ToggleGroup group =new ToggleGroup();
+        RadioButton male = new RadioButton("Male");
+        male.setToggleGroup(group);
+        RadioButton female =new RadioButton("Female");
+        female.setToggleGroup(group);
+        stat.setString(4,group.toString());
+        stat.setString(5,NIC.getText());
+        stat.setString(6,pin.getText());
+        stat.setString(7,AccountNo.getText());
+        stat.setString(8,AccountType.getText());
+        stat.setString(9,Ini_Deposit.getText());
+        stat.setString(10,Phone_No.getText());
+        stat.setString(11,Address.getText());
+        stat.execute();
+        stat.close();
+        dataBaselink.close();
 
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
